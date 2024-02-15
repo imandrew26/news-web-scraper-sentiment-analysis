9163514f3e3d1fe8972ac977938817bef878b45e
imandrew26 <imandrew@umich.edu>
2024-02-15 02:03:53 -0500

    v1.0.1

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup;
 import requests;
 import pandas as pd;
+import nltk;
+from collections import namedtuple;
+from textblob import TextBlob;
 
 url = "https://www.nbcnews.com/us-news"
 url2 = "https://abcnews.go.com/US"
@@ -15,7 +18,7 @@ result2 = requests.get(url2, headers=agent)
 nbcDoc = BeautifulSoup(result.text, "html.parser")
 abcDoc = BeautifulSoup(result2.text, "html.parser")
 
-articlesAndLinks = [] # Stores articles and links as tuples
+
 
 article_titlesA = nbcDoc.find_all('h2', class_="styles_headline__ice3t") # NBC articles from lastest news, top stories, politics
 article_titlesB = nbcDoc.find_all('h2', class_="wide-tease-item__headline") # NBC filler articles
@@ -23,27 +26,38 @@ article_titlesB = nbcDoc.find_all('h2', class_="wide-tease-item__headline") # NB
 article_titlesC = abcDoc.find_all('a', class_="AnchorLink News__Item external flex flex-row")
 print(len(article_titlesC))
 
+Article = namedtuple('Article', ['title', 'link', 'polarity'])
+articlesAndLinks = [] # Stores articles and links as tuples
+updatedArticlesAndLinks = []
 
 for title in article_titlesA:
     if title.find('a'):
-        title_text = title.get_text()
-        link = title.find('a')['href']
-        articlesAndLinks.append((title_text, link))
+        tempArticle = Article(title=title.get_text(), link=title.find('a')['href'], polarity=0)
+        articlesAndLinks.append(tempArticle)
 
 for title in article_titlesB:
     if title.find('a'):
-        title_text = title.get_text()
-        link = title.find('a')['href']
-        articlesAndLinks.append((title_text, link))
+
+        tempArticle = Article(title=title.get_text(), link=title.find('a')['href'], polarity=0)
+        articlesAndLinks.append(tempArticle)
 
 for title in article_titlesC:
-        title_text = title.get_text()
-        link = title.get('href')
-        articlesAndLinks.append((title_text, link))
+        
+        tempArticle = Article(title=title.get_text(), link=title.get('href'), polarity=0)
+        articlesAndLinks.append(tempArticle)
+
+for article in articlesAndLinks:
+    title, link, polarity = article
+    blob = TextBlob(title)
+    newPolarity = blob.sentiment.polarity
+    updatedArticle = Article(title, link, newPolarity)
+    updatedArticlesAndLinks.append(updatedArticle)
 
 
-df = pd.DataFrame(articlesAndLinks, columns = ['Title', 'Link'])
+
+df = pd.DataFrame(updatedArticlesAndLinks, columns = ['Title', 'Link', 'Polarity'])
 print(df)
+
 
 
 #testpush
