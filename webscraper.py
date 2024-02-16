633e9914931a8f3fd6798546281427d47949f6bd
imandrew26 <imandrew@umich.edu>
2024-02-15 21:10:08 -0500

    v1.0.2

--- webscraper.py
+++ webscraper.py
@@ -1,9 +1,12 @@
-from bs4 import BeautifulSoup;
 import requests;
 import pandas as pd;
+import re;
 import nltk;
 from collections import namedtuple;
 from textblob import TextBlob;
+from transformers import BertTokenizer, BertModel
+from bs4 import BeautifulSoup;
+
 
 url = "https://www.nbcnews.com/us-news"
 url2 = "https://abcnews.go.com/US"
@@ -24,38 +27,44 @@ article_titlesA = nbcDoc.find_all('h2', class_="styles_headline__ice3t") # NBC a
 article_titlesB = nbcDoc.find_all('h2', class_="wide-tease-item__headline") # NBC filler articles
 
 article_titlesC = abcDoc.find_all('a', class_="AnchorLink News__Item external flex flex-row")
-print(len(article_titlesC))
 
-Article = namedtuple('Article', ['title', 'link', 'polarity'])
+Article = namedtuple('Article', ['title', 'preprocessedTitle', 'link', 'polarity'])
 articlesAndLinks = [] # Stores articles and links as tuples
 updatedArticlesAndLinks = []
 
 for title in article_titlesA:
     if title.find('a'):
-        tempArticle = Article(title=title.get_text(), link=title.find('a')['href'], polarity=0)
+        tempArticle = Article(title=title.get_text(), preprocessedTitle=title.get_text(), link=title.find('a')['href'], polarity=0)
         articlesAndLinks.append(tempArticle)
 
 for title in article_titlesB:
     if title.find('a'):
 
-        tempArticle = Article(title=title.get_text(), link=title.find('a')['href'], polarity=0)
+        tempArticle = Article(title=title.get_text(), preprocessedTitle=title.get_text(), link=title.find('a')['href'], polarity=0)
         articlesAndLinks.append(tempArticle)
 
 for title in article_titlesC:
         
-        tempArticle = Article(title=title.get_text(), link=title.get('href'), polarity=0)
+        tempArticle = Article(title=title.get_text(), preprocessedTitle=title.get_text(), link=title.get('href'), polarity=0)
         articlesAndLinks.append(tempArticle)
 
+# text proprocessing using TextBlob
 for article in articlesAndLinks:
-    title, link, polarity = article
-    blob = TextBlob(title)
+    title, preprocessedTitle, link, polarity = article
+
+    # lowercase and remove punctuation
+    preprocessedTitle = preprocessedTitle.lower()
+    preprocessedTitle = re.sub(r'[^\w\s]', '', str(preprocessedTitle))
+
+    blob = TextBlob(preprocessedTitle)
     newPolarity = blob.sentiment.polarity
-    updatedArticle = Article(title, link, newPolarity)
+
+    updatedArticle = Article(title, preprocessedTitle, link, newPolarity)
     updatedArticlesAndLinks.append(updatedArticle)
 
 
 
-df = pd.DataFrame(updatedArticlesAndLinks, columns = ['Title', 'Link', 'Polarity'])
+df = pd.DataFrame(updatedArticlesAndLinks, columns = ['Title', 'Preprocessed Title', 'Link', 'Polarity'])
 print(df)
 
 
